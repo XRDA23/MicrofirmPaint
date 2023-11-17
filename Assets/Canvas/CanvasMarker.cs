865e6586b81799ba9013edce561b712eb87e315d
LoredanaCicati <74156276+LoredanaCicati@users.noreply.github.com>
2023-11-17 14:34:56 +0100

    refactor code for CanvasMarker script

--- Assets/Canvas/CanvasMarker.cs
+++ Assets/Canvas/CanvasMarker.cs
@@ -28,51 +28,58 @@ public class CanvasMarker : MonoBehaviour
 
     void Update()
     {
-        Draw();
+        ProcessDrawing();
     }
 
-    private void Draw()
+    private void ProcessDrawing()
     {
-        if (Physics.Raycast(_tip.position, transform.up, out _touch, _tipHeight))
+        if (Physics.Raycast(_tip.position, transform.up, out _touch, _tipHeight) && _touch.transform.CompareTag("Canvas"))
         {
-            if (_touch.transform.CompareTag("Canvas"))
-            {
-                if (_canvas == null)
-                {
-                    _canvas = _touch.transform.GetComponent<Canvas>();
-                }
+            HandleCanvasTouch();
+        }
+        else
+        {
+            _touchedLastFrame = false;
+            _canvas = null;
+        }
+    }
 
-                _touchPos = new Vector2(_touch.textureCoord.x, _touch.textureCoord.y);
-
-                var x = (int)(_touchPos.x * _canvas.textureSize.x - (_penSize / 2));
-                var y = (int)(_touchPos.y * _canvas.textureSize.y - (_penSize / 2));
-
-                if (y < 0 || y > _canvas.textureSize.y || x < 0 || x > _canvas.textureSize.x) return;
-
-                if (_touchedLastFrame)
-                {
-                    _canvas.texture.SetPixels(x, y, _penSize, _penSize, _colors);
-
-                    for (float f = 0.01f; f < 1.00f; f += 0.01f)
-                    {
-                        var lerpX = (int)Mathf.Lerp(_lastTouchPos.x, x, f);
-                        var lerpY = (int)Mathf.Lerp(_lastTouchPos.y, y, f);
-                        _canvas.texture.SetPixels(lerpX, lerpY, _penSize, _penSize, _colors);
-                    }
-
-                    transform.rotation = _lastTouchRot;
-                    
-                    _canvas.texture.Apply();
-                }
-
-                _lastTouchPos = new Vector2(x, y);
-                _lastTouchRot = transform.rotation;
-                _touchedLastFrame = true;
-                return;
-            }
+    private void HandleCanvasTouch()
+    {
+        if (_canvas == null)
+        {
+            _canvas = _touch.transform.GetComponent<Canvas>();
         }
 
-        _canvas = null;
-        _touchedLastFrame = false;
+        _touchPos = new Vector2(_touch.textureCoord.x, _touch.textureCoord.y);
+
+        var x = (int)(_touchPos.x * _canvas.textureSize.x - (_penSize / 2));
+        var y = (int)(_touchPos.y * _canvas.textureSize.y - (_penSize / 2));
+
+        if (x < 0 || x >= _canvas.textureSize.x || y < 0 || y >= _canvas.textureSize.y) return;
+
+        DrawPixels(x, y);
+
+        _lastTouchPos = new Vector2(x, y);
+        _lastTouchRot = transform.rotation;
+        _touchedLastFrame = true;
+    }
+
+    private void DrawPixels(int x, int y)
+    {
+        _canvas.texture.SetPixels(x, y, _penSize, _penSize, _colors);
+
+        if (_touchedLastFrame)
+        {
+            for (float f = 0.01f; f < 1.00f; f += 0.01f)
+            {
+                var lerpX = (int)Mathf.Lerp(_lastTouchPos.x, x, f);
+                var lerpY = (int)Mathf.Lerp(_lastTouchPos.y, y, f);
+                _canvas.texture.SetPixels(lerpX, lerpY, _penSize, _penSize, _colors);
+            }
+        }
+        
+        _canvas.texture.Apply();
     }
 }
+
