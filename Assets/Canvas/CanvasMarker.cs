dbe05409a8a48082d78148e15f479a8225e2286d
LoredanaCicati <74156276+LoredanaCicati@users.noreply.github.com>
2023-11-17 14:41:08 +0100

    add functionality for switch color tip of marker

--- Assets/Canvas/CanvasMarker.cs
+++ Assets/Canvas/CanvasMarker.cs
@@ -1,85 +1,114 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
-using Unity.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CanvasMarker : MonoBehaviour
 {
-    [SerializeField] private Transform _tip;
-    [SerializeField] private int _penSize = 5;
+   [SerializeField] private Transform _tip;
+   [SerializeField] private int _penSize = 5;
+   [SerializeField] private Color[] penColors; 
 
-    private Renderer _renderer;
-    private Color[] _colors;
-    private float _tipHeight;
+   private Renderer _renderer;
+   private Color[] _colors;
+   private float _tipHeight;
 
-    private RaycastHit _touch;
-    private Canvas _canvas;
-    private Vector2 _touchPos, _lastTouchPos;
-    private bool _touchedLastFrame;
-    private Quaternion _lastTouchRot;
-    
-    void Start()
-    {
-        _renderer = _tip.GetComponent<Renderer>();
-        _colors = Enumerable.Repeat(_renderer.material.color, _penSize * _penSize).ToArray();
-        _tipHeight = _tip.localScale.y;
-    }
+   private RaycastHit _touch;
+   private Canvas _canvas;
+   private Vector2 _touchPos, _lastTouchPos;
+   private bool _touchedLastFrame;
+   private Quaternion _lastTouchRot;
+   private int currentColorIndex = 0;
 
-    void Update()
-    {
-        ProcessDrawing();
-    }
+   
+   public InputActionReference colorChangeAction;
 
-    private void ProcessDrawing()
-    {
-        if (Physics.Raycast(_tip.position, transform.up, out _touch, _tipHeight) && _touch.transform.CompareTag("Canvas"))
-        {
-            HandleCanvasTouch();
-        }
-        else
-        {
-            _touchedLastFrame = false;
-            _canvas = null;
-        }
-    }
+   void Start()
+   {
+       _renderer = _tip.GetComponent<Renderer>();
+       _tipHeight = _tip.localScale.y;
+       // Initialize with the first color
+       SetColor(penColors[currentColorIndex]); 
+       
+       colorChangeAction.action.performed += OnColorChangeActionPerformed;
+       colorChangeAction.action.Enable();
+   }
 
-    private void HandleCanvasTouch()
-    {
-        if (_canvas == null)
-        {
-            _canvas = _touch.transform.GetComponent<Canvas>();
-        }
+   void OnDestroy()
+   {
+       colorChangeAction.action.performed -= OnColorChangeActionPerformed;
+   }
 
-        _touchPos = new Vector2(_touch.textureCoord.x, _touch.textureCoord.y);
+   void Update()
+   {
+       ProcessDrawing();
+   }
 
-        var x = (int)(_touchPos.x * _canvas.textureSize.x - (_penSize / 2));
-        var y = (int)(_touchPos.y * _canvas.textureSize.y - (_penSize / 2));
+   private void OnColorChangeActionPerformed(InputAction.CallbackContext context)
+   {
+       SwitchColor();
+   }
 
-        if (x < 0 || x >= _canvas.textureSize.x || y < 0 || y >= _canvas.textureSize.y) return;
+   private void SwitchColor()
+   {
+       currentColorIndex = (currentColorIndex + 1) % penColors.Length;
+       SetColor(penColors[currentColorIndex]);
+   }
 
-        DrawPixels(x, y);
+   public void SetColor(Color newColor)
+   {
+       // Change the material color of the tip
+       _renderer.material.color = newColor; 
+       // Update the color
+       _colors = Enumerable.Repeat(newColor, _penSize * _penSize).ToArray(); 
+   }
+   private void ProcessDrawing()
+   {
+       if (Physics.Raycast(_tip.position, transform.up, out _touch, _tipHeight) && _touch.transform.CompareTag("Canvas"))
+       {
+           HandleCanvasTouch();
+       }
+       else
+       {
+           _touchedLastFrame = false;
+           _canvas = null;
+       }
+   }
 
-        _lastTouchPos = new Vector2(x, y);
-        _lastTouchRot = transform.rotation;
-        _touchedLastFrame = true;
-    }
+   private void HandleCanvasTouch()
+   {
+       if (_canvas == null)
+       {
+           _canvas = _touch.transform.GetComponent<Canvas>();
+       }
 
-    private void DrawPixels(int x, int y)
-    {
-        _canvas.texture.SetPixels(x, y, _penSize, _penSize, _colors);
+       _touchPos = new Vector2(_touch.textureCoord.x, _touch.textureCoord.y);
 
-        if (_touchedLastFrame)
-        {
-            for (float f = 0.01f; f < 1.00f; f += 0.01f)
-            {
-                var lerpX = (int)Mathf.Lerp(_lastTouchPos.x, x, f);
-                var lerpY = (int)Mathf.Lerp(_lastTouchPos.y, y, f);
-                _canvas.texture.SetPixels(lerpX, lerpY, _penSize, _penSize, _colors);
-            }
-        }
-        
-        _canvas.texture.Apply();
-    }
+       var x = (int)(_touchPos.x * _canvas.textureSize.x - (_penSize / 2));
+       var y = (int)(_touchPos.y * _canvas.textureSize.y - (_penSize / 2));
+
+       if (x < 0 || x >= _canvas.textureSize.x || y < 0 || y >= _canvas.textureSize.y) return;
+
+       DrawPixels(x, y);
+
+       _lastTouchPos = new Vector2(x, y);
+       _lastTouchRot = transform.rotation;
+       _touchedLastFrame = true;
+   }
+
+   private void DrawPixels(int x, int y)
+   {
+       _canvas.texture.SetPixels(x, y, _penSize, _penSize, _colors);
+
+       if (_touchedLastFrame)
+       {
+           for (float f = 0.01f; f < 1.00f; f += 0.01f)
+           {
+               var lerpX = (int)Mathf.Lerp(_lastTouchPos.x, x, f);
+               var lerpY = (int)Mathf.Lerp(_lastTouchPos.y, y, f);
+               _canvas.texture.SetPixels(lerpX, lerpY, _penSize, _penSize, _colors);
+           }
+       }
+      
+       _canvas.texture.Apply();
+   }
 }
-
