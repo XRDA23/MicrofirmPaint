c53bcdf16329da676f1a05e0c2bd46bf5e1fd7bb
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2023-11-18 14:57:04 +0100

    Only make canvas white once
    - Froze eraser rotation

--- Assets/Scripts/Canvas/CanvasScript.cs
+++ Assets/Scripts/Canvas/CanvasScript.cs
@@ -7,14 +7,20 @@ namespace Canvas
     {
         public Texture2D texture;
         public Vector2 textureSize = new Vector2(2048, 2048);
+        private bool isWhite = false;
 
         void Start()
         {
             var r = GetComponent<Renderer>();
             texture = new Texture2D((int)textureSize.x, (int)textureSize.y);
             r.material.mainTexture = texture;
+            if (isWhite)
+            {
+                return;
+            }
             texture.SetPixels(Enumerable.Repeat(Color.white, (int) textureSize.x * (int) textureSize.y).ToArray());
             texture.Apply();
+            isWhite = true;
         }
     }
 }
