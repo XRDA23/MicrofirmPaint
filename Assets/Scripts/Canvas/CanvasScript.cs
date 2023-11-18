29d8f5555526a17752041a846d13f203a2c54832
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2023-11-18 13:36:07 +0100

    Unfreezing eraser rotation
    - Moving canvas scripts into scripts folder
    - Renaming canvas script due to naming conflict

---
+++ Assets/Scripts/Canvas/CanvasScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Canvas
+{
+    public class CanvasScript : MonoBehaviour
+    {
+        public Texture2D texture;
+        public Vector2 textureSize = new Vector2(2048, 2048);
+
+        void Start()
+        {
+            var r = GetComponent<Renderer>();
+            texture = new Texture2D((int)textureSize.x, (int)textureSize.y);
+            r.material.mainTexture = texture;
+        }
+    }
+}
