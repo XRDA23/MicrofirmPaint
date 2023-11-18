82fac22ed928ca8dc20184a542334951ce6f239f
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2023-11-18 13:45:25 +0100

    Revert "Revert "Unfreezing eraser rotation""
    This reverts commit 845fbe0962162cf48c22bbf8095a7afefc6b651c.

--- Assets/Scripts/CanvasEraser.cs
+++ Assets/Scripts/CanvasEraser.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using Canvas;
 using UnityEngine;
 
 public class CanvasEraser : MonoBehaviour
@@ -12,7 +13,7 @@ public class CanvasEraser : MonoBehaviour
     private float _tipHeight;
 
     private RaycastHit _touch;
-    private Canvas _canvas;
+    private CanvasScript canvasScript;
     private Vector2 _touchPos, _lastTouchPos;
     private bool _touchedLastFrame;
     private Quaternion _lastTouchRot;
@@ -33,32 +34,32 @@ public class CanvasEraser : MonoBehaviour
     {
         if (Physics.Raycast(_tip.position, transform.up, out _touch, _tipHeight))
         {
-            if (_touch.transform.CompareTag("Canvas"))
+            if (_touch.transform.CompareTag("CanvasScript"))
             {
-                if (_canvas == null)
+                if (canvasScript == null)
                 {
-                    _canvas = _touch.transform.GetComponent<Canvas>();
+                    canvasScript = _touch.transform.GetComponent<CanvasScript>();
                 }
 
                 _touchPos = new Vector2(_touch.textureCoord.x, _touch.textureCoord.y);
 
-                var x = (int)(_touchPos.x * _canvas.textureSize.x - (_eraserSize / 2));
-                var y = (int)(_touchPos.y * _canvas.textureSize.y - (_eraserSize / 2));
+                var x = (int)(_touchPos.x * canvasScript.textureSize.x - (_eraserSize / 2));
+                var y = (int)(_touchPos.y * canvasScript.textureSize.y - (_eraserSize / 2));
 
-                if (y < 0 || y > _canvas.textureSize.y || x < 0 || x > _canvas.textureSize.x) return;
+                if (y < 0 || y > canvasScript.textureSize.y || x < 0 || x > canvasScript.textureSize.x) return;
 
                 if (_touchedLastFrame)
                 {
-                    _canvas.texture.SetPixels(x, y, _eraserSize, _eraserSize, _clearColors);
+                    canvasScript.texture.SetPixels(x, y, _eraserSize, _eraserSize, _clearColors);
 
                     for (float f = 0.01f; f < 1.00f; f += 0.01f)
                     {
                         var lerpX = (int)Mathf.Lerp(_lastTouchPos.x, x, f);
                         var lerpY = (int)Mathf.Lerp(_lastTouchPos.y, y, f);
-                        _canvas.texture.SetPixels(lerpX, lerpY, _eraserSize, _eraserSize, _clearColors);
+                        canvasScript.texture.SetPixels(lerpX, lerpY, _eraserSize, _eraserSize, _clearColors);
                     }
 
-                    _canvas.texture.Apply();
+                    canvasScript.texture.Apply();
                 }
 
                 _lastTouchPos = new Vector2(x, y);
@@ -68,7 +69,7 @@ public class CanvasEraser : MonoBehaviour
             }
         }
 
-        _canvas = null;
+        canvasScript = null;
         _touchedLastFrame = false;
     }
 }
